47fb7b8ce1293cdfc7d85d50667ada95bdca937f
camilavargasp <cvargas@bren.ucsb.edu>
2024-06-24 10:13:03 -0700

    adding read and minor wrangle of data to global.R

--- dashboard-app/global.R
+++ dashboard-app/global.R
@@ -3,19 +3,21 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(leaflet)
-
+library(bivariatechoropleths)
 
 
 # READ DATA ----
+prop_fail_area <- read_sf("data/mock_prob_fail_levee_area.shp") %>% 
+  mutate(island_tract = paste0("island_", 1:77)) 
 
-mock_data_levee <- read_sf("data/mock-data-levee-area.shp")
+soc_vul_index <- read_csv("data/mock_social_vul_index.csv")
 
+levee_area_data <- prop_fail_area %>% 
+  left_join(soc_vul_index, by = "island_tract") %>% 
+  mutate(fail_percentile = percent_rank(lev_flr),
+         soc_vul_percentile = percent_rank(social_vul),
+         lma = str_to_title(lma))
 
-risk_soc_data <- mock_data_levee %>% 
-  select(island_name = LMA, 
-         island_tract = islnd_t,
-         social_vul = socl_vl,
-         risk,
-         risk_scaled_percent = rsk_sc_)
-
+fail_soc_wgs84 <- levee_area_data %>% 
+  st_transform(crs = 4326) 
 
