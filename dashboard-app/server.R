24fdb654fe11f0fce2f45be6d52907ec9a901f27
camilavargasp <cvargas@bren.ucsb.edu>
2024-07-23 15:25:48 -0700

    adding code to create soc vul plot and prob of fail independently.Check box input is not quite working.

--- dashboard-app/server.R
+++ dashboard-app/server.R
@@ -5,10 +5,83 @@ server <- function(input, output){
   # render leaflet map ----
   output$leaflet_map <- renderLeaflet({
    
-    if(input$map_choice == "soc_vul_map"){
+    if(input$map_choice == "soc_vul_map"){ # OPEN IF STATEMENT
+      
+      # Create a color palette for the soc_vul_map:
+      soc_vul_pal <- colorNumeric(
+        palette = "viridis", 
+        domain = fail_soc_wgs84$soc_vul_percentile,
+        na.color = "gray")
+      
+      # labels for islands in soc_vul_map
+      soc_vul_labs <- paste0("<b>",fail_soc_wgs84$lma,"</b>", "</br>",
+                             "<br><b>Social Vulnerability Index</b>",
+                             "<br>",round(fail_soc_wgs84$soc_vul_percentile,3), "</br>",
+                             "<br><b>","Probabiliy of Failure</b>",
+                             "<br>", round(fail_soc_wgs84$fail_percentile, 3))
       
       
-    } 
+      soc_vul_labs <- purrr::map(soc_vul_labs, htmltools::HTML)
+      
+      leaflet::leaflet(fail_soc_wgs84) %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        addPolygons(data = fail_soc_wgs84,
+                    fillColor = ~soc_vul_pal(soc_vul_percentile),
+                    label = soc_vul_labs,
+                    stroke = TRUE,
+                    fillOpacity = 0.6, 
+                    color="black", # polygon border color
+                    weight=0.8, ) %>% # polygon border weight
+        addLegend(pal = soc_vul_pal,
+                  values = ~soc_vul_percentile,
+                  opacity = 0.6,
+                  title = "Social Vulnerability Index",
+                  position = "bottomleft") %>% 
+        setView(lat=38.2, lng=-121.7, zoom=9)
+    
+      } #END 1st IF
+    
+    else if(input$map_choice == "imapct_map") {  #start 2nd IF
+      
+        #"NEED TO UPDATE TO IMPACT MAP"
+        
+        # Create a color palette for the soc_vul_map:
+        prob_fail_pal <- colorNumeric(
+          palette = "viridis", 
+          domain = fail_soc_wgs84$fail_percentile,
+          na.color = "grey")
+        
+        # labels for islands in soc_vul_map
+        prob_fail_labs <- paste0("<b>",fail_soc_wgs84$lma,"</b>", "</br>",
+                                 "<br><b>","Probabiliy of Failure</b>",
+                                 "<br>", round(fail_soc_wgs84$fail_percentile, 3), "</br>",
+                                 "<br><b>Social Vulnerability Index</b>",
+                                 "<br>",round(fail_soc_wgs84$soc_vul_percentile,3))
+        
+        
+        prob_fail_labs <- purrr::map(prob_fail_labs, htmltools::HTML)
+        
+        leaflet::leaflet(fail_soc_wgs84) %>%
+          addProviderTiles("CartoDB.Positron") %>%
+          addPolygons(data = fail_soc_wgs84,
+                      fillColor = ~prob_fail_pal(fail_percentile),
+                      label = prob_fail_labs,
+                      stroke = TRUE,
+                      fillOpacity = 0.6, 
+                      color="black", # polygon border color
+                      weight=0.8, ) %>% # polygon border weight
+          addLegend(pal = prob_fail_pal,
+                    values = ~fail_percentile,
+                    opacity = 0.6,
+                    title = "Probability of Failure",
+                    position = "bottomleft") %>% 
+          setView(lat=38.2, lng=-121.7, zoom=9)
+        
+        
+    } #END 2nd IF 
+    
+    else if(input$map_choice == "imapct_map" & input$map_choice == "soc_vul_map") { 
+    
     
     # create leaflet bichoropleth
     leaflet::leaflet() %>%
@@ -28,6 +101,7 @@ server <- function(input, output){
         highlightOptions = leaflet::highlightOptions(color = "orange",
                                                      weight = 2,
                                                      opacity = 1)) 
+    }
     
   }) # END of leaflet map
   
