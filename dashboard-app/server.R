c978f2707a54e7817612024160b5c0295e70e2f2
camilavargasp <cvargas@bren.ucsb.edu>
2024-09-27 01:21:35 +0000

    updating leaflet layer map with code with all layers

--- dashboard-app/server.R
+++ dashboard-app/server.R
@@ -22,6 +22,13 @@ server <- function(input, output){
       addProviderTiles("Esri.WorldImagery", 
                        group = "Imagery") %>%
       ## Add all polygon layers
+      addPolygons(data = levee_areas,
+                  group = "Levee Areas",
+                  label = ~htmlEscape(
+                    paste("Name:", LMA)),
+                  color="black", # polygon border color
+                  fill = NA,
+                  weight=1.2) %>% 
       addPolygons(data = soc_vul,
                   group = "Social Vulnerability",
                   fillColor = ~soc_vul_pal(RPL_THEMES),
@@ -31,13 +38,13 @@ server <- function(input, output){
                   stroke = TRUE,
                   fillOpacity = 0.6, 
                   color="black", # polygon border color
-                  weight=0.8, ) %>% # polygon border weight
-      addPolygons(data = prob_fail_w84,
+                  weight=0.8 ) %>% # polygon border weight
+      addPolygons(data = prob_fail,
                   group = "Probability of Failure",
                   fillColor = ~prob_fail_pal(lev_flr),
                   # label = soc_vul_labs,
                   stroke = TRUE,
-                  fillOpacity = 0.6, 
+                  fillOpacity = 0.6,
                   color="black", # polygon border color
                   weight=0.8, ) %>% # polygon border weight
       addPolygons(data = managed_wetlands,
@@ -45,17 +52,25 @@ server <- function(input, output){
                   fillColor = ~managed_wetlands_pal(PM_LndT),
                   # label = soc_vul_labs,
                   stroke = TRUE,
-                  fillOpacity = 0.6, 
+                  fillOpacity = 0.6,
                   color="black", # polygon border color
                   weight=0.8, ) %>% # polygon border weight
-      addPolygons(data = croplands_complete,
+      addPolygons(data = croplands,
                   group = "Croplands",
                   fillColor = ~crops_pal(type),
                   # label = soc_vul_labs,
                   stroke = TRUE,
-                  fillOpacity = 0.6, 
+                  fillOpacity = 0.6,
                   color="black", # polygon border color
                   weight=0.8, ) %>% # polygon border weight
+      addPolygons(data = structure_value,
+                  group = "Structure",
+                  fillColor = ~structure_pal(total_value_trill),
+                  # label = soc_vul_labs,
+                  stroke = TRUE,
+                  fillOpacity = 0.6,
+                  color = "black", # remove polygon border
+                  weight=0.8, ) %>%
       ## Legend layers
       addLegend(group = "Social Vulnerability",
                 pal = soc_vul_pal,
@@ -65,7 +80,7 @@ server <- function(input, output){
                 position = "bottomleft") %>%
       addLegend(group = "Probability of Failure",
                 pal = prob_fail_pal,
-                values = prob_fail_w84$lev_flr,
+                values = prob_fail$lev_flr,
                 opacity = 0.6,
                 title = "Probability of Failure",
                 position = "bottomleft") %>%
@@ -74,18 +89,34 @@ server <- function(input, output){
                 values = managed_wetlands$PM_LndT,
                 opacity = 0.6,
                 title = "Habitat Type",
-                position = "bottomleft") %>% 
+                position = "bottomleft") %>%
+      addLegend(group = "Croplands",
+                pal = crops_pal,
+                values = croplands$type,
+                opacity = 0.6,
+                title = "Cropland Group",
+                position = "bottomleft") %>%
+      addLegend(group = "Structure",
+                pal = structure_pal,
+                values = structure_value$total_value_trill,
+                opacity = 0.6,
+                title = "Structure Value (Trillions)",
+                position = "bottomleft") %>%
       addLayersControl(
         baseGroups = c("Grey background", "Imagery"),
-        overlayGroups = c("Social Vulnerability",
+        overlayGroups = c("Levee Areas",
+                          "Social Vulnerability",
                           "Probability of Failure",
-                          "Managed Wetlands"),
+                          "Habitat Type",
+                          "Croplands",
+                          "Structure"),
         options = layersControlOptions(collapsed = FALSE)) %>%
       # hide these groups by default
       hideGroup(c("Social Vulnerability",
                   "Probability of Failure",
                   "Habitat Type",
-                  "Croplands")) %>% 
+                  "Croplands",
+                  "Structure")) %>%
       setView(lat=38.2, lng=-121.7, zoom=9)
 
     
